6d1d051b2cd147827a1ecc565eb33449c74eb3c6
Daniel Molin <dkd333@gmail.com>
2018-04-23 22:32:32 +0200

    added lines do stations

--- src/constants.js
+++ src/constants.js
@@ -111,531 +111,613 @@ export const stationsFull = [
     {
         "origName": "Slussen (Stockholm)",
         "siteId": "9192",
-        "name": "Slussen"
+        "name": "Slussen",
+        "lines": [10, 11, 13, 14, 17, 18, 19]
     },
     {
         "origName": "Medborgarplatsen (Stockholm)",
         "siteId": "1323",
-        "name": "Medborgarplatsen"
+        "name": "Medborgarplatsen",
+        "lines": [17, 18, 19]
     },
     {
         "origName": "Skanstull (Stockholm)",
         "siteId": "9190",
-        "name": "Skanstull"
+        "name": "Skanstull",
+        "lines": [17, 18, 19]
     },
     {
         "origName": "Gullmarsplan (Stockholm)",
         "siteId": "9189",
-        "name": "Gullmarsplan"
+        "name": "Gullmarsplan",
+        "lines": [17, 18, 19]
     },
     {
         "origName": "Skärmarbrink (Stockholm)",
         "siteId": "9188",
-        "name": "Skärmarbrink"
+        "name": "Skärmarbrink",
+        "lines": [17, 18]
     },
     {
         "origName": "Blåsut (Stockholm)",
         "siteId": "9187",
-        "name": "Blåsut"
+        "name": "Blåsut",
+        "lines": [18]
     },
     {
         "origName": "Sandsborg (Stockholm)",
         "siteId": "9186",
-        "name": "Sandsborg"
+        "name": "Sandsborg",
+        "lines": [18]
     },
     {
         "origName": "Skogskyrkogården (Stockholm)",
         "siteId": "9185",
-        "name": "Skogskyrkogården"
+        "name": "Skogskyrkogården",
+        "lines": [18]
     },
     {
         "origName": "Tallkrogen (Stockholm)",
         "siteId": "9184",
-        "name": "Tallkrogen"
+        "name": "Tallkrogen",
+        "lines": [18]
     },
     {
         "origName": "Gubbängen (Stockholm)",
         "siteId": "9183",
-        "name": "Gubbängen"
+        "name": "Gubbängen",
+        "lines": [18]
     },
     {
         "origName": "Hökarängen (Stockholm)",
         "siteId": "9182",
-        "name": "Hökarängen"
+        "name": "Hökarängen",
+        "lines": [18]
     },
     {
         "origName": "Globen (Stockholm)",
         "siteId": "9168",
-        "name": "Globen"
+        "name": "Globen",
+        "lines": [19]
     },
     {
         "origName": "Enskede gård (Stockholm)",
         "siteId": "9167",
-        "name": "Enskede gård"
+        "name": "Enskede gård",
+        "lines": [19]
     },
     {
         "origName": "Sockenplan (Stockholm)",
         "siteId": "9166",
-        "name": "Sockenplan"
+        "name": "Sockenplan",
+        "lines": [19]
     },
     {
         "origName": "Svedmyra (Stockholm)",
         "siteId": "9165",
-        "name": "Svedmyra"
+        "name": "Svedmyra",
+        "lines": [19]
     },
     {
         "origName": "Stureby (Stockholm)",
         "siteId": "9164",
-        "name": "Stureby"
+        "name": "Stureby",
+        "lines": [19]
     },
     {
         "origName": "Hötorget (Stockholm)",
         "siteId": "9119",
-        "name": "Hötorget"
+        "name": "Hötorget",
+        "lines": [17, 18, 19]
     },
     {
         "origName": "Rådmansgatan (t-banan) (Stockholm)",
         "siteId": "9118",
-        "name": "Rådmansgatan"
+        "name": "Rådmansgatan",
+        "lines": [17, 18, 19]
     },
     {
         "origName": "Odenplan (Stockholm)",
         "siteId": "9117",
-        "name": "Odenplan"
+        "name": "Odenplan",
+        "lines": [17, 18, 19]
     },
     {
         "origName": "Sankt Eriksplan (Stockholm)",
         "siteId": "9116",
-        "name": "Sankt Eriksplan"
+        "name": "Sankt Eriksplan",
+        "lines": [17, 18, 19]
     },
     {
         "origName": "Fridhemsplan (Stockholm)",
         "siteId": "9115",
-        "name": "Fridhemsplan"
+        "name": "Fridhemsplan",
+        "lines": [10, 11, 17, 18, 19]
     },
     {
         "origName": "Thorildsplan (Stockholm)",
         "siteId": "9114",
-        "name": "Thorildsplan"
+        "name": "Thorildsplan",
+        "lines": [17, 18, 19]
     },
     {
         "origName": "Kristineberg (Stockholm)",
         "siteId": "9113",
-        "name": "Kristineberg"
+        "name": "Kristineberg",
+        "lines": [17, 18, 19]
     },
     {
         "origName": "Alvik (Stockholm)",
         "siteId": "9112",
-        "name": "Alvik"
+        "name": "Alvik",
+        "lines": [17, 18, 19]
     },
     {
         "origName": "Stora mossen (Stockholm)",
         "siteId": "9111",
-        "name": "Stora mossen"
+        "name": "Stora mossen",
+        "lines": [17, 19]
     },
     {
         "origName": "Abrahamsberg (Stockholm)",
         "siteId": "9110",
-        "name": "Abrahamsberg"
+        "name": "Abrahamsberg",
+        "lines": [17, 19]
     },
     {
         "origName": "Brommaplan (Stockholm)",
         "siteId": "9109",
-        "name": "Brommaplan"
+        "name": "Brommaplan",
+        "lines": [17, 19]
     },
     {
         "origName": "Åkeshov (Stockholm)",
         "siteId": "9108",
-        "name": "Åkeshov"
+        "name": "Åkeshov",
+        "lines": [17, 19]
     },
     {
         "origName": "Ängbyplan (Stockholm)",
         "siteId": "9107",
-        "name": "Ängbyplan"
+        "name": "Ängbyplan",
+        "lines": [19]
     },
     {
         "origName": "Islandstorget (Stockholm)",
         "siteId": "9106",
-        "name": "Islandstorget"
+        "name": "Islandstorget",
+        "lines": [19]
     },
     {
         "origName": "Blackeberg (Stockholm)",
         "siteId": "9105",
-        "name": "Blackeberg"
+        "name": "Blackeberg",
+        "lines": [19]
     },
     {
         "origName": "Råcksta (Stockholm)",
         "siteId": "9104",
-        "name": "Råcksta"
+        "name": "Råcksta",
+        "lines": [19]
     },
     {
         "origName": "Vällingby (Stockholm)",
         "siteId": "9103",
-        "name": "Vällingby"
+        "name": "Vällingby",
+        "lines": [19]
     },
     {
         "origName": "Bandhagen (Stockholm)",
         "siteId": "9163",
-        "name": "Bandhagen"
+        "name": "Bandhagen",
+        "lines": [19]
     },
     {
         "origName": "Högdalen (Stockholm)",
         "siteId": "9162",
-        "name": "Högdalen"
+        "name": "Högdalen",
+        "lines": [19]
     },
     {
         "origName": "Johannelund (Stockholm)",
         "siteId": "9102",
-        "name": "Johannelund"
+        "name": "Johannelund",
+        "lines": [19]
     },
     {
         "origName": "Hässelby gård (Stockholm)",
         "siteId": "9101",
-        "name": "Hässelby gård"
+        "name": "Hässelby gård",
+        "lines": [19]
     },
     {
         "origName": "T-Centralen (Stockholm)",
         "siteId": "9001",
-        "name": "T-Centralen"
+        "name": "T-Centralen",
+        "lines": [10, 11, 13, 14, 17, 18, 19]
     },
     {
         "origName": "Gamla stan (Stockholm)",
         "siteId": "9193",
-        "name": "Gamla stan"
+        "name": "Gamla stan",
+        "lines": [10, 11, 13, 14, 17, 18, 19]
     },
     {
         "origName": "Hammarbyhöjden (Stockholm)",
         "siteId": "9144",
-        "name": "Hammarbyhöjden"
+        "name": "Hammarbyhöjden",
+        "lines": [17]
     },
     {
         "origName": "Björkhagen (Stockholm)",
         "siteId": "9143",
-        "name": "Björkhagen"
+        "name": "Björkhagen",
+        "lines": [17]
     },
     {
         "origName": "Kärrtorp (Stockholm)",
         "siteId": "9142",
-        "name": "Kärrtorp"
+        "name": "Kärrtorp",
+        "lines": [17]
     },
     {
         "origName": "Bagarmossen (Stockholm)",
         "siteId": "9141",
-        "name": "Bagarmossen"
+        "name": "Bagarmossen",
+        "lines": [17]
     },
     {
-        "origName": "Farsta strand (Stockholm)",
-        "siteId": "9180",
-        "name": "Farsta"
+        "origName": "Farsta",
+        "siteId": "9181",
+        "name": "Farsta",
+        "lines": [18]
     },
     {
         "origName": "Hässelby strand (Stockholm)",
         "siteId": "9100",
-        "name": "Hässelby strand"
+        "name": "Hässelby strand",
+        "lines": [19]
     },
     {
         "origName": "Rågsved (Stockholm)",
         "siteId": "9161",
-        "name": "Rågsved"
+        "name": "Rågsved",
+        "lines": [19]
     },
     {
         "origName": "Hagsätra (Stockholm)",
         "siteId": "9160",
-        "name": "Hagsätra"
-    },
-    {
-        "origName": "T-Centralen (Stockholm)",
-        "siteId": "9001",
-        "name": "T-Centralen"
-    },
-    {
-        "origName": "Gamla stan (Stockholm)",
-        "siteId": "9193",
-        "name": "Gamla stan"
-    },
-    {
-        "origName": "Slussen (Stockholm)",
-        "siteId": "9192",
-        "name": "Slussen"
+        "name": "Hagsätra",
+        "lines": [19]
     },
     {
         "origName": "Zinkensdamm (Stockholm)",
         "siteId": "9296",
-        "name": "Zinkensdamm"
+        "name": "Zinkensdamm",
+        "lines": [13, 14]
     },
     {
         "origName": "Hornstull (Stockholm)",
         "siteId": "9295",
-        "name": "Hornstull"
+        "name": "Hornstull",
+        "lines": [13, 14]
     },
     {
         "origName": "Liljeholmen (Stockholm)",
         "siteId": "9294",
-        "name": "Liljeholmen"
+        "name": "Liljeholmen",
+        "lines": [13, 14]
     },
     {
         "origName": "Midsommarkransen (Stockholm)",
         "siteId": "9264",
-        "name": "Midsommarkransen"
+        "name": "Midsommarkransen",
+        "lines": [14]
     },
     {
         "origName": "Telefonplan (Stockholm)",
         "siteId": "9263",
-        "name": "Telefonplan"
+        "name": "Telefonplan",
+        "lines": [14]
     },
     {
         "origName": "Mariatorget (t-banan) (Stockholm)",
         "siteId": "9297",
-        "name": "Mariatorget"
+        "name": "Mariatorget",
+        "lines": [13, 14]
     },
     {
         "origName": "Hägerstensåsen (t-banan) (Stockholm)",
         "siteId": "9262",
-        "name": "Hägerstensåsen"
+        "name": "Hägerstensåsen",
+        "lines": [14]
     },
     {
         "origName": "Västertorp (Stockholm)",
         "siteId": "9261",
-        "name": "Västertorp"
+        "name": "Västertorp",
+        "lines": [14]
     },
     {
         "origName": "Fruängen (Stockholm)",
         "siteId": "9260",
-        "name": "Fruängen"
+        "name": "Fruängen",
+        "lines": [14]
     },
     {
         "origName": "Aspudden (Stockholm)",
         "siteId": "9293",
-        "name": "Aspudden"
+        "name": "Aspudden",
+        "lines": [13]
     },
     {
         "origName": "Örnsberg (Stockholm)",
         "siteId": "9292",
-        "name": "Örnsberg"
+        "name": "Örnsberg",
+        "lines": [13]
     },
     {
         "origName": "Axelsberg (Stockholm)",
         "siteId": "9291",
-        "name": "Axelsberg"
+        "name": "Axelsberg",
+        "lines": [13]
     },
     {
         "origName": "Mälarhöjden (Stockholm)",
         "siteId": "9290",
-        "name": "Mälarhöjden"
+        "name": "Mälarhöjden",
+        "lines": [13]
     },
     {
         "origName": "Bredäng (Stockholm)",
         "siteId": "9289",
-        "name": "Bredäng"
+        "name": "Bredäng",
+        "lines": [13]
     },
     {
         "origName": "Sätra (Stockholm)",
         "siteId": "9288",
-        "name": "Sätra"
+        "name": "Sätra",
+        "lines": [13]
     },
     {
         "origName": "Östermalmstorg (Stockholm)",
         "siteId": "9206",
-        "name": "Östermalmstorg"
+        "name": "Östermalmstorg",
+        "lines": [13, 14]
     },
     {
         "origName": "Karlaplan (Stockholm)",
         "siteId": "9222",
-        "name": "Karlaplan"
+        "name": "Karlaplan",
+        "lines": [13]
     },
     {
         "origName": "Gärdet (Stockholm)",
         "siteId": "9221",
-        "name": "Gärdet"
+        "name": "Gärdet",
+        "lines": [13]
     },
     {
         "origName": "Ropsten (Stockholm)",
         "siteId": "9220",
-        "name": "Ropsten"
+        "name": "Ropsten",
+        "lines": [13]
     },
     {
         "origName": "Skärholmen (Stockholm)",
         "siteId": "9287",
-        "name": "Skärholmen"
+        "name": "Skärholmen",
+        "lines": [13]
     },
     {
         "origName": "Vårberg (Stockholm)",
         "siteId": "9286",
-        "name": "Vårberg"
+        "name": "Vårberg",
+        "lines": [13]
     },
     {
         "origName": "Farsta strand (Stockholm)",
         "siteId": "9180",
-        "name": "Farsta strand"
+        "name": "Farsta strand",
+        "lines": [18]
     },
     {
         "origName": "Vårby gård (Huddinge)",
         "siteId": "9285",
-        "name": "Vårby gård"
+        "name": "Vårby gård",
+        "lines": [13]
     },
     {
         "origName": "Masmo (Huddinge)",
         "siteId": "9284",
-        "name": "Masmo"
+        "name": "Masmo",
+        "lines": [13]
     },
     {
         "origName": "Fittja (Botkyrka)",
         "siteId": "9283",
-        "name": "Fittja"
+        "name": "Fittja",
+        "lines": [13]
     },
     {
         "origName": "Stadion (t-banan) (Stockholm)",
         "siteId": "9205",
-        "name": "Stadion"
+        "name": "Stadion",
+        "lines": [14]
     },
     {
         "origName": "Tekniska högskolan (Stockholm)",
         "siteId": "9204",
-        "name": "Tekniska högskolan"
+        "name": "Tekniska högskolan",
+        "lines": [14]
     },
     {
         "origName": "Universitetet (Stockholm)",
         "siteId": "9203",
-        "name": "Universitetet"
+        "name": "Universitetet",
+        "lines": [14]
     },
     {
         "origName": "Alby (Botkyrka)",
         "siteId": "9282",
-        "name": "Alby"
+        "name": "Alby",
+        "lines": [13]
     },
     {
         "origName": "Hallunda (Botkyrka)",
         "siteId": "9281",
-        "name": "Hallunda"
+        "name": "Hallunda",
+        "lines": [13]
     },
     {
         "origName": "Norsborg (Botkyrka)",
         "siteId": "9280",
-        "name": "Norsborg"
-    },
-    {
-        "origName": "T-Centralen (Stockholm)",
-        "siteId": "9001",
-        "name": "T-Centralen"
+        "name": "Norsborg",
+        "lines": [13]
     },
     {
         "origName": "Rådhuset (Stockholm)",
         "siteId": "9309",
-        "name": "Rådhuset"
+        "name": "Rådhuset",
+        "lines": [10, 11]
     },
     {
         "origName": "Fridhemsplan (Stockholm)",
         "siteId": "9115",
-        "name": "Fridhemsplan"
+        "name": "Fridhemsplan",
+        "lines": [10, 11, 17, 18, 19]
     },
     {
         "origName": "Stadshagen (Stockholm)",
         "siteId": "9307",
-        "name": "Stadshagen"
+        "name": "Stadshagen",
+        "lines": [10, 11]
     },
     {
         "origName": "Västra skogen (Solna)",
         "siteId": "9306",
-        "name": "Västra skogen"
+        "name": "Västra skogen",
+        "lines": [10, 11]
     },
     {
         "origName": "Solna centrum (Solna)",
         "siteId": "9305",
-        "name": "Solna centrum"
+        "name": "Solna centrum",
+        "lines": [11]
     },
     {
         "origName": "Näckrosen (Solna)",
         "siteId": "9304",
-        "name": "Näckrosen"
+        "name": "Näckrosen",
+        "lines": [11]
     },
     {
         "origName": "Hallonbergen (Sundbyberg)",
         "siteId": "9303",
-        "name": "Hallonbergen"
+        "name": "Hallonbergen",
+        "lines": [11]
     },
     {
         "origName": "Rinkeby (Stockholm)",
         "siteId": "9322",
-        "name": "Rinkeby"
+        "name": "Rinkeby",
+        "lines": [10]
     },
     {
         "origName": "Tensta (Stockholm)",
         "siteId": "9321",
-        "name": "Tensta"
+        "name": "Tensta",
+        "lines": [10]
     },
     {
         "origName": "Hjulsta (Stockholm)",
         "siteId": "9320",
-        "name": "Hjulsta"
+        "name": "Hjulsta",
+        "lines": [10]
     },
     {
         "origName": "Kymmendö (Haninge)",
         "siteId": "225",
-        "name": "Kymlinge"
+        "name": "Kymlinge",
+        "lines": [11]
     },
     {
         "origName": "Kista (Stockholm)",
         "siteId": "9302",
-        "name": "Kista"
+        "name": "Kista",
+        "lines": [11]
     },
     {
         "origName": "Husby (Stockholm)",
         "siteId": "9301",
-        "name": "Husby"
+        "name": "Husby",
+        "lines": [11]
     },
     {
         "origName": "Akalla (Stockholm)",
         "siteId": "9300",
-        "name": "Akalla"
+        "name": "Akalla",
+        "lines": [11]
     },
     {
         "origName": "Kungsträdgården (tunnelbana) (Stockholm)",
         "siteId": "9340",
-        "name": "Kungsträdgården"
+        "name": "Kungsträdgården",
+        "lines": [10, 11]
     },
     {
         "origName": "Bergshamra (Solna)",
         "siteId": "9202",
-        "name": "Bergshamra"
+        "name": "Bergshamra",
+        "lines": [14]
     },
     {
         "origName": "Danderyds sjukhus (Danderyd)",
         "siteId": "9201",
-        "name": "Danderyds sjukhus"
+        "name": "Danderyds sjukhus",
+        "lines": [14]
     },
     {
         "origName": "Mörby centrum (Danderyd)",
         "siteId": "9200",
-        "name": "Mörby centrum"
+        "name": "Mörby centrum",
+        "lines": [14]
     },
     {
         "origName": "Huvudsta (Solna)",
         "siteId": "9327",
-        "name": "Huvudsta"
+        "name": "Huvudsta",
+        "lines": [11]
     },
     {
         "origName": "Solna strand (Solna)",
         "siteId": "9326",
-        "name": "Solna strand"
+        "name": "Solna strand",
+        "lines": [11]
     },
     {
         "origName": "Sundbybergs centrum (Sundbyberg)",
         "siteId": "9325",
-        "name": "Sundbybergs centrum"
+        "name": "Sundbybergs centrum",
+        "lines": [11]
     },
     {
         "origName": "Duvbo (Sundbyberg)",
         "siteId": "9324",
-        "name": "Duvbo"
+        "name": "Duvbo",
+        "lines": [11]
     },
     {
         "origName": "Rissne (Sundbyberg)",
         "siteId": "9323",
-        "name": "Rissne"
+        "name": "Rissne",
+        "lines": [11]
     },
     {
         "origName": "Skarpnäck (t-banan) (Stockholm)",
         "siteId": "9140",
-        "name": "Skarpnäck"
+        "name": "Skarpnäck",
+        "lines": [17]
     }
 ]
