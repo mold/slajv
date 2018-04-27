5e85dde359341f64bc4907fc8795aa157648b774
Johan Jansson <Johan.Jansson@axakon.se>
2018-04-27 19:14:11 +0200

    Now, all trains on line 11 are found and printed with respect to the progress between last and next station.

--- src/index.js
+++ src/index.js
@@ -31,40 +31,74 @@ function getStationsOnSpecificLine(lineNumber, constantStations) {
 let line11 = getStationsOnSpecificLine(11, stationsFull);
 
 line11.then(trains => {
-  trains.forEach(trainArray => {
-    //console.log(trainArray);
-  })
-  let temp = _.orderBy(trains[1], ['expectedMilliseconds'],['asc']);
-  console.log("temp: ", temp);
+  console.log("Number of trains: ", trains.length);
+  console.log("trains: ", trains);
+
   var myVar = setInterval(myTimer, 1000);
   function myTimer() {
 
-    let passedStations = temp.filter(train => train.expectedMilliseconds < moment().valueOf()); // stations already passed by the train
-    let lastPassedStation = passedStations[passedStations.length - 1]; // last station
+    trains.forEach(stationArray => {
+      let temp = _.orderBy(stationArray, ['expectedMilliseconds'],['asc']);
 
-    let comingStations = temp.filter(train => train.expectedMilliseconds > moment().valueOf()); // stations already passed by the train
+      let passedStations = temp.filter(train => train.expectedMilliseconds < moment().valueOf()); // stations already passed by the train
+      let lastPassedStation = passedStations[passedStations.length - 1]; // last station
 
-    if (lastPassedStation && comingStations.length > 0) {
-      let stationTimeDifference = comingStations[0].expectedMilliseconds - lastPassedStation.expectedMilliseconds;
-      let timeUntilStation = comingStations[0].expectedMilliseconds - moment().valueOf();
-      let progress = 100*(1-(timeUntilStation/stationTimeDifference));
+      let comingStations = temp.filter(train => train.expectedMilliseconds > moment().valueOf()); // stations already passed by the train
 
-      console.log("From: " + lastPassedStation.StopAreaName + " To: " + comingStations[0].StopAreaName + " Progress: " + progress + "%");
-      console.log("From: " + moment(lastPassedStation.expectedMilliseconds).format("DD MMM YYYY hh:mm a") + " To: " + moment(comingStations[0].expectedMilliseconds).format("DD MMM YYYY hh:mm a"));
+      if (lastPassedStation && comingStations.length > 0) {
+        let stationTimeDifference = comingStations[0].expectedMilliseconds - lastPassedStation.expectedMilliseconds;
+        let timeUntilStation = comingStations[0].expectedMilliseconds - moment().valueOf();
+        let progress = 100*(1-(timeUntilStation/stationTimeDifference));
 
-      // comingStations.forEach((train, i) => {
-      //   let stationTimeDifference = train.expectedMilliseconds - lastPassedStation.expectedMilliseconds;
-      //   let timeUntilStation = train.expectedMilliseconds - moment().valueOf();
-      //   let progress = 100*stationTimeDifference/(1-timeUntilStation);
-      //   console.log("From: " + lastPassedStation.StopAreaName + " To: " + train.StopAreaName + " Progress: " + progress + "%");
-      //   //
-      //   // if (moment().isAfter(moment(train.TimeTabledDateTime))) {
-      //   //   console.log(moment(train.TimeTabledDateTime).fromNow());
-      //   // }
-      // });
-    }
-
+        console.log("From: " + lastPassedStation.StopAreaName + " To: " + comingStations[0].StopAreaName + " Progress: " + progress + "%");
+        // console.log("From: " + moment(lastPassedStation.expectedMilliseconds).format("DD MMM YYYY hh:mm a") + " To: " + moment(comingStations[0].expectedMilliseconds).format("DD MMM YYYY hh:mm a"));
+      }
+    })
   }
+
+
+
+
 });
+
+
+// line11.then(trains => {
+//   console.log("Number of trains: ", trains.length);
+//   console.log("trains: ", trains);
+//   trains.forEach(stationArray => {
+//     // do something
+//   })
+//   let temp = _.orderBy(trains[1], ['expectedMilliseconds'],['asc']);
+//   console.log("temp: ", temp);
+//   var myVar = setInterval(myTimer, 1000);
+//   function myTimer() {
+//
+//     let passedStations = temp.filter(train => train.expectedMilliseconds < moment().valueOf()); // stations already passed by the train
+//     let lastPassedStation = passedStations[passedStations.length - 1]; // last station
+//
+//     let comingStations = temp.filter(train => train.expectedMilliseconds > moment().valueOf()); // stations already passed by the train
+//
+//     if (lastPassedStation && comingStations.length > 0) {
+//       let stationTimeDifference = comingStations[0].expectedMilliseconds - lastPassedStation.expectedMilliseconds;
+//       let timeUntilStation = comingStations[0].expectedMilliseconds - moment().valueOf();
+//       let progress = 100*(1-(timeUntilStation/stationTimeDifference));
+//
+//       console.log("From: " + lastPassedStation.StopAreaName + " To: " + comingStations[0].StopAreaName + " Progress: " + progress + "%");
+//       console.log("From: " + moment(lastPassedStation.expectedMilliseconds).format("DD MMM YYYY hh:mm a") + " To: " + moment(comingStations[0].expectedMilliseconds).format("DD MMM YYYY hh:mm a"));
+//
+//       // comingStations.forEach((train, i) => {
+//       //   let stationTimeDifference = train.expectedMilliseconds - lastPassedStation.expectedMilliseconds;
+//       //   let timeUntilStation = train.expectedMilliseconds - moment().valueOf();
+//       //   let progress = 100*stationTimeDifference/(1-timeUntilStation);
+//       //   console.log("From: " + lastPassedStation.StopAreaName + " To: " + train.StopAreaName + " Progress: " + progress + "%");
+//       //   //
+//       //   // if (moment().isAfter(moment(train.TimeTabledDateTime))) {
+//       //   //   console.log(moment(train.TimeTabledDateTime).fromNow());
+//       //   // }
+//       // });
+//     }
+//
+//   }
+// });
 
 document.getElementById('root').innerHTML = "hej";
