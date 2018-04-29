73e5130572bc2c5d743e505d189e7f6bd97e9c70
Johan Jansson <Johan.Jansson@axakon.se>
2018-04-29 22:52:11 +0200

    a lot of progress in getting a nice output for the map. Need to check what is still to be done.

--- src/index.js
+++ src/index.js
@@ -15,16 +15,21 @@ function getStationsOnSpecificLine(lineNumber, constantStations) {
     let promiseArr = line.map(station => getStationTrains(station.siteId));
 
     return Promise.all(promiseArr).then(stations => {
-      var filteredStations = _.flatten(stations).filter(station => station.LineNumber == lineNumber.toString())
+      var filteredStations = _.flatten(stations).filter(station => station.LineNumber == lineNumber.toString());
+
       filteredStations = filteredStations.map(station => {
         station.expectedMilliseconds = moment(station.ExpectedDateTime).valueOf();
         return station;
       });
+
       var uniqueTrains = _.uniqBy(filteredStations, "JourneyNumber");
       var uniqueJurneyNumbers = uniqueTrains.map(train => train.JourneyNumber);
-      var temp2 = [];
-      uniqueJurneyNumbers.forEach(journeyNumber => {temp2.push(filteredStations.filter(train => train.JourneyNumber === journeyNumber))});
-      return temp2;
+
+      var functionOutput = [];
+      uniqueJurneyNumbers.forEach(journeyNumber => {
+        functionOutput.push(filteredStations.filter(train => train.JourneyNumber === journeyNumber));
+      });
+      return functionOutput;
     })
 }
 
@@ -34,10 +39,13 @@ line11.then(trains => {
   console.log("Number of trains: ", trains.length);
   console.log("trains: ", trains);
 
+  let trainsIntransit = [];
+
   var myVar = setInterval(myTimer, 1000);
   function myTimer() {
-
-    trains.forEach(stationArray => {
+    let timeStamp = moment().valueOf(); // used for determining if a train has stopped at the last station on the line
+    
+    trains.forEach(stationArray => { // loop through all the trains stations
       let temp = _.orderBy(stationArray, ['expectedMilliseconds'],['asc']);
 
       let passedStations = temp.filter(train => train.expectedMilliseconds < moment().valueOf()); // stations already passed by the train
@@ -45,15 +53,43 @@ line11.then(trains => {
 
       let comingStations = temp.filter(train => train.expectedMilliseconds > moment().valueOf()); // stations already passed by the train
 
-      if (lastPassedStation && comingStations.length > 0) {
+      if (lastPassedStation && comingStations.length > 0) { // the current train is between two stations
         let stationTimeDifference = comingStations[0].expectedMilliseconds - lastPassedStation.expectedMilliseconds;
         let timeUntilStation = comingStations[0].expectedMilliseconds - moment().valueOf();
         let progress = 100*(1-(timeUntilStation/stationTimeDifference));
 
-        console.log("From: " + lastPassedStation.StopAreaName + " To: " + comingStations[0].StopAreaName + " Progress: " + progress + "%");
+        var index = _.findIndex(trainsIntransit, {journeyNumber: comingStations[0].JourneyNumber});
+        let trainToAdd = {timeStamp: timeStamp, journeyNumber: comingStations[0].JourneyNumber, from: lastPassedStation.StopAreaName, to: comingStations[0].StopAreaName, progress: progress};
+
+        // Add/modify the current train to/in the output array
+        if (index >= 0) { // train already exists in array
+          trainsIntransit.splice(index, 1, trainToAdd);
+        } else { // new train
+          trainsIntransit.push(trainToAdd);
+        }
+
+        /* The stations for trains in the array "trainsIntransit" will be
+         * overwritten by new stations ass the tranin passes the current
+         * station.
+         *
+         * In the case that the api has not provided information of another
+         * station where the specific train will depart from, the train object
+         * will have a ".progress" which is <= 100%. This case has to be
+         * handled for "end stations". Other cases should automatically work
+         * if the api continously is called and provides information on stations.
+         */
+
+
+
+        // console.log("From: " + lastPassedStation.StopAreaName + " To: " + comingStations[0].StopAreaName + " Progress: " + progress + "%");
         // console.log("From: " + moment(lastPassedStation.expectedMilliseconds).format("DD MMM YYYY hh:mm a") + " To: " + moment(comingStations[0].expectedMilliseconds).format("DD MMM YYYY hh:mm a"));
       }
     })
+
+    if (trainsIntransit.length > 1) { // naïve and flawed fix for the problem stated above.
+      let temp = trainsIntransit.filter(train => train.timeStamp === (_.maxBy(trainsIntransit, 'timeStamp')).timeStamp);
+      console.log(temp);
+    }
   }
 
 
